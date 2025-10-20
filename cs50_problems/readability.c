e24736e6492159801b569625e9553c12aab19189
Zhevryn-mr <abdullogafurov2007@gmail.com>
2025-10-20 15:43:17 +0300

    Add files via upload

---
+++ cs50_problems/readability.c
@@ -0,0 +1,124 @@
+., [20.10.2025 7:40]
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+bool only_digits(string s);
+char rotate(char c, int n);
+
+int main(int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+
+    if (!only_digits(argv[1]))
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+
+    int key = atoi(argv[1]);
+    string plaintext = get_string("plaintext:  ");
+
+    printf("ciphertext: ");
+    for (int i = 0, len = strlen(plaintext); i < len; i++)
+    {
+        printf("%c", rotate(plaintext[i], key));
+    }
+    printf("\n");
+    return 0;
+}
+
+bool only_digits(string s)
+{
+    for (int i = 0, len = strlen(s); i < len; i++)
+    {
+        if (!isdigit(s[i]))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+char rotate(char c, int n)
+{
+    if (isupper(c))
+    {
+        return ((c - 'A' + n) % 26) + 'A';
+    }
+    else if (islower(c))
+    {
+        return ((c - 'a' + n) % 26) + 'a';
+    }
+    else
+    {
+        return c;
+    }
+}
+
+., [20.10.2025 7:41]
+#include <cs50.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int count_letters(string text);
+int count_words(string text);
+int count_sentences(string text);
+
+int main(void)
+{
+    string text = get_string("Text: ");
+    int letters = count_letters(text);
+    int words = count_words(text);
+    int sentences = count_sentences(text);
+
+    float L = (float)letters / words * 100;
+    float S = (float)sentences / words * 100;
+    float index = 0.0588 * L - 0.296 * S - 15.8;
+
+    int grade = round(index);
+    
+    if (grade < 1) printf("Before Grade 1\n");
+    else if (grade >= 16) printf("Grade 16+\n");
+    else printf("Grade %d\n", grade);
+    
+    return 0;
+}
+
+int count_letters(string text)
+{
+    int count = 0;
+    for (int i = 0, len = strlen(text); i < len; i++)
+    {
+        if (isalpha(text[i])) count++;
+    }
+    return count;
+}
+
+int count_words(string text)
+{
+    int count = 0;
+    for (int i = 0, len = strlen(text); i < len; i++)
+    {
+        if (isspace(text[i])) count++;
+    }
+    return count + 1;
+}
+
+int count_sentences(string text)
+{
+    int count = 0;
+    for (int i = 0, len = strlen(text); i < len; i++)
+    {
+        if (text[i] == '.'  text[i] == '!'  text[i] == '?') count++;
+    }
+    return count;
+}
